050c07c96e52e5ae3b5420763d97939c273f3c8b
harshith <hgurupra@cisco.com>
2018-10-25 00:38:37 -0700

    Created login and user registration

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://HarshithGP:SJBHS2012HGP@ds141633.mlab.com:41633/josephites"
+    "mongodb://HarshithGP:SJBHS2012HGP@ds141633.mlab.com:41633/josephites",
+  secretKey: "f1de_et_lab0re"
 };
